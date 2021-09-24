f7e60b73f8d95f5f066c1b2c3cd560bbab0c7c9e
Haughtdawg <rhaughton18@cmc.edu>
2021-09-23 17:14:55 -0700

    Finished up AddTaskContainer but still needs to be styled.

--- src/Components/TodoBoardComponents/AddTaskContainer.js
+++ src/Components/TodoBoardComponents/AddTaskContainer.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export function AddTaskContainer(){
 
+    const [taskName, setTaskName] = useState('');
+    // Jkld;sa
     return(
-        <div>
-            <span><FontAwesomeIcon icon={ faPlus } /></span>
-            <span></span>
-            <span></span>
+        <div className="AddTaskContainer d-flex ">
+
+            {/* Break out into its own class */}
+            <span className="d-flex flex-fill justify-content-center">
+                <Button onClick={()=>alert(taskName)}>
+                    <FontAwesomeIcon icon={ faPlus } />
+                </Button>
+            </span>
+            
+            {/* Break out into its own class */}
+            <span className="d-flex flex-fill justify-content-center">
+                <Form>
+                    <Form.Label>Add Task</Form.Label>
+                    <Form.Control type="text" placeholder="Input task name here" onChange = {e => setTaskName(e.target.value)} value={taskName}></Form.Control>
+                </Form>
+            </span>
+            
+            {/* Break out into its own class */}
+            <span className="d-flex flex-fill justify-content-center">
+                <Button variant="danger">
+                    <FontAwesomeIcon icon={ faTrash } />
+                </Button>
+            </span>
         </div>
     )
 }
