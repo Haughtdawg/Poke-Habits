50d62c3558b3353f4204d6735b51d7b3311ca9fa
Haughtdawg <rhaughton18@cmc.edu>
2021-10-13 16:58:33 -0700

    Added tab nav navigation

---
+++ src/Components/PointsPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../index.css';
+
+
+/*
+    Inputs: 
+    State variables: 
+    Parents: 
+    Children:
+*/
+
+export function PointsPage(){
+    
+
+    return(
+        <div>
+            <h1>Pooints</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { TodoBoard } from './Components/TodoBoard.js'
+import { TodoBoard } from './Components/TodoBoard.js';
+import { PointsPage } from './Components/PointsPage.js';
 import './index.css';
 
 function TodoPage(){
+    const [window, setWindow] = useState('home')
+    console.log(window === 'home')
     // imported from https://react-bootstrap.github.io/components/navs/
     return(
         <div>
-            <Nav variant="tabs" defaultActiveKey="/home">
+            <Nav variant="tabs" defaultActiveKey="/home" onSelect={eventKey => setWindow(eventKey)}>
                 <Nav.Item>
-                    <Nav.Link href="/home">Todo</Nav.Link>
+                    <Nav.Link eventKey='home'>Todo</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-1">Points</Nav.Link>
+                    <Nav.Link eventKey="points">Points</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="link-2">Collection</Nav.Link>
+                    <Nav.Link eventKey="collections">Collection</Nav.Link>
                 </Nav.Item>
             </Nav>
             <h1>Poke-Habits</h1>
-            <TodoBoard/>            
+            {(window==='home')&&<TodoBoard/>}
+            {(window ==='points')&&<PointsPage/>}          
         </div>
     )
 };
